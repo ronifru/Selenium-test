687b5d2bd4ca21f431d3b07003222b88f2b0206e
ronifru <ronifru08@gmail.com>
2017-06-27 01:52:07 +0300

    finished

--- src/automationFramework/Flights.java
+++ src/automationFramework/Flights.java
@@ -3,7 +3,6 @@ package automationFramework;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -13,27 +12,35 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Flights {
 	
-	public static void main (String []args) throws IOException{
+	public static void main (String []args) throws Exception{
 		
 		File file = new File("flights.txt");
-		file.createNewFile();
-		FileWriter fw = new FileWriter(file);
 		String row = "";
-		
 		WebDriver driver = new ChromeDriver();
 		String URL = "http://www.iaa.gov.il/he-IL/airports/BenGurion/Pages/OnlineFlights.aspx";
+		
 		driver.get(URL);
-		
-		WebElement baseTable = driver.findElement(By.className("generalTable"));
-		List<WebElement> tableRows = baseTable.findElements(By.tagName("tr"));
-		for (int i=1; i < tableRows.size(); i++){
-			row = tableRows.get(i).getText();
-			fw.write(row);
-			fw.flush();
-			System.out.println(row);
-			
+		while(true){
+			if (isPageLoaded(driver)){
+				FileWriter fw = new FileWriter("flights.txt", false);
+				WebElement baseTable = driver.findElement(By.className("generalTable"));
+				List<WebElement> tableRows = baseTable.findElements(By.tagName("tr"));
+				for (int i=1; i < tableRows.size(); i++){
+					List<WebElement> rowCells = tableRows.get(i).findElements(By.tagName("td"));
+					fw.write("flight number: " + rowCells.get(2).getText() + " from: " + rowCells.get(3).getText() + " scheduled: " + rowCells.get(4).getText() +
+							" updated: " +  rowCells.get(5).getText() + " terminal: " + rowCells.get(6).getText() + " status: " + rowCells.get(7).getText() + "\n");
+					fw.flush();
+				}
+				Thread.sleep(60000*5);
+				driver.navigate().refresh();	
+			}
+			else {
+				Thread.sleep(1000);
+			}
 		}
-		
 	}
-
+	
+	public static boolean isPageLoaded (WebDriver driver){
+		return driver.findElements(By.className("odd")).size() > 0; 
+	}
 }
